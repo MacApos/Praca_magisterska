afff1702f4285a99ce6b0bed0b87ea40dc84a51e
MacApo <maciejapostol98@gmail.com>
2022-07-28 01:25:40 +0200

    commit_07.27

--- lane_detection/camera_calibration_02.py
+++ lane_detection/camera_calibration_02.py
@@ -73,3 +73,4 @@ plt.show()
 # Zapis macierzy kamery i współczynników dystorsji w celu ponownego użytku
 pickle.dump(mtx, open(r'data/data_array/mtx.p', 'wb'))
 pickle.dump(dist, open('data/data_array/dist.p', 'wb'))
+
